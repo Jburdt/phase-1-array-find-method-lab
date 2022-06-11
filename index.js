c2b85d90b589560929b49c1f3faf23be3a4a6cdc
Josh Burd <Jburdt@gmail.com>
2022-06-11 18:08:16 -0400

    commit tree 1f060c293fc2668fb2cb07f3a0ff359a2fd76945

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+const win = record.find(element => element.result === "W")
+  if (win) {
+    return win.year
+  }else return undefined
+}
+
+
